14e15b498f9d3f7cc8585fe1e2b91ec8a7d76962
rohit-the-webdev <107198004+rohit-the-webdev@users.noreply.github.com>
2026-03-14 10:19:07 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = conn.connections[0].readyState;
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log("MongoDB Error:", error.message);
-    process.exit(1);
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 };
 
