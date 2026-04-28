4b809000574f9bae5d0215e72a18b4eb6f8025a8
rohit-the-webdev <rohitkale842@gmail.com>
2026-04-28 15:29:58 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -23,7 +23,13 @@ console.log({
   notificationRoutes,
   activityRoutes,
 });
+const cors = require("cors");
 
+app.use(cors({
+  origin: "https://stpm-client.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
