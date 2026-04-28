5cdb35b911baf97459e2b83598ecd858074a772b
rohit-the-webdev <rohitkale842@gmail.com>
2026-04-28 15:15:13 +0530

    server.js chanfed

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
