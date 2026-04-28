a77c21185ccc23d5a97bd80583008071f322daf7
rohit-the-webdev <rohitkale842@gmail.com>
2026-04-28 15:50:54 +0530

    minor fix

--- api/index.js
+++ api/index.js
@@ -4,6 +4,14 @@ const connectDB = require("../src/config/db");
 const app = require("../src/app");
 
 module.exports = async (req, res) => {
-    await connectDB();
-    return app(req, res);
+    try {
+        await connectDB();
+        return app(req, res);
+    } catch (error) {
+        console.error("Vercel Entry Point Error:", error);
+        return res.status(500).json({
+            error: "Internal Server Error",
+            message: error.message,
+        });
+    }
 };
